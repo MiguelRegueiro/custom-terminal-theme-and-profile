7fd5760f37d9a22c9bec926ad5389f21b78c5d9a
Miguel Pérez Regueiro <67692547+MiguelRegueiro@users.noreply.github.com>
2024-11-03 18:14:35 +0100

    Add files via upload
    Updated with welcome message and color changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
 oh-my-posh init pwsh --config 'C:\Users\migue\AppData\Local\Programs\oh-my-posh\themes\agnoster.ompRegueiro.json' | Invoke-Expression
 Import-Module Terminal-Icons
 Set-PSReadLineOption -PredictionViewStyle ListView
+
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#6A9955' }
+Write-Host "🖥️ Welcome back, InsertUserName. Let's get to work." -ForegroundColor Cyan
+
+function Reload-Profile { . $PROFILE }
+Set-Alias reload Reload-Profile
