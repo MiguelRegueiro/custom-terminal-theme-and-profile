a46c3c87cb6c3a63b22c50075072f7c6a6e50dd2
Miguel Pérez Regueiro <67692547+MiguelRegueiro@users.noreply.github.com>
2024-11-03 17:45:44 +0100

    Add files via upload
    Add custom terminal configuration with Oh My Posh theme and PowerShell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+oh-my-posh init pwsh --config 'C:\Users\migue\AppData\Local\Programs\oh-my-posh\themes\agnoster.omp.json' | Invoke-Expression
+Import-Module Terminal-Icons
+Set-PSReadLineOption -PredictionViewStyle ListView
